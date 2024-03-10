dcbb3f9e1f2eda672dcc069e2fc371b19c8dfd61
AGBORNTUI BATE <87149550+charlodevops@users.noreply.github.com>
2024-03-10 12:50:15 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,20 @@
 variable "ipam_name" {
-  type = string
+  default = "global-ipam"
+  type    = string
+}
+
+variable "global_cidr" {
+  default = "10.0.0.0/8"
+  type    = string
 }
 
 variable "operating_regions" {
   type = map(string)
-}
-
-variable "global_cidr" {
-  type = string
+  default = {
+    "us-east-1" = "10.1.0.0/16"
+    "us-west-1" = "10.2.0.0/16"
+    "eu-west-1" = "10.3.0.0/16"
+  }
 }
 
 variable "default_netmask" {
